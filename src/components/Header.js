40a0194450a81b36b3af16643ebcdaa9264dca9c
akash gokral <akashgokral1@gmail.com>
2022-03-20 01:57:16 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../components/Home/styles/header.css'
-
 import { FaTh } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 import { useState } from "react";
 import Mobilenavigation from './Mobilenavigation';
 import Navigation from './Navigation';
@@ -10,21 +10,26 @@ const Header = () => {
 
   const [open, setopen] = useState(false);
 
-
+  const hamburgerIcon = <FaTh className='hamburger' onClick={() => {
+    setopen(!open)
+  }} />
+  const closeIcon = <ImCross className='hamburger' onClick={() => {
+    setopen(!open)
+  }} />
 
   return <div>
     <div className='header_1'>
-      <div className='header'>
-        <span>The</span><h1> Siren </h1>
+
+      <div className='header'><span>The</span><h1> Siren </h1>  </div>
+
+      <div className='icon' >
+        {open ? closeIcon : hamburgerIcon}
       </div>
 
-      <div className='icon' ><FaTh className='hamburger' onClick={() => {
-        setopen(!open)
-      }} /> </div>
     </div>
     <Navigation />
     {open && <Mobilenavigation />}
-    {/* <Nav /> */}
+
 
 
   </div>;
